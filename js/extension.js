ddbd3f2fb195120da70c2ec2d0a5525a3cbeaeac
Siddharth Bulia <siddharthbulia@meeshos-MacBook-Pro.local>
2019-11-05 10:15:37 +0530

    Deleting files from main folder

--- js/extension.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    if(url !==  "https://leetcode.com/api/problems/algorithms/"){
-      var newURL = "https://leetcode.com/api/problems/algorithms/";
-      chrome.tabs.create({ url: newURL });
-    }
-  });
-}, false);
